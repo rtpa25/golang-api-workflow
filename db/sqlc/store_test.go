1b1f2be763667ac0dd674015a33b843566301e2f
rtpa25 <pandaronit25@gmail.com>
2022-03-19 10:31:26 +0530

    init ci workflow

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -97,4 +97,64 @@ func TestTransferTxn(t *testing.T) {
 		k := int(diff1 / amount)
 		assert.True(t, k >= 1 && k <= n)
 	}
+
+	//check the final updated balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	assert.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	assert.NoError(t, err)
+
+	fmt.Println(">> After:", updatedAccount1.Balance, updatedAccount2.Balance)
+
+	assert.Equal(t, account1.Balance-int64(n)*amount, updatedAccount1.Balance)
+	assert.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
+}
+
+func TestTransferTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createRadnomAccount(t)
+	account2 := createRadnomAccount(t)
+	fmt.Println(">> before:", account1.Balance, account2.Balance)
+
+	n := 10
+	amount := int64(10)
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		err := <-errs
+		assert.NoError(t, err)
+	}
+
+	// check the final updated balance
+	updatedAccount1, err := store.GetAccount(context.Background(), account1.ID)
+	assert.NoError(t, err)
+
+	updatedAccount2, err := store.GetAccount(context.Background(), account2.ID)
+	assert.NoError(t, err)
+
+	fmt.Println(">> after:", updatedAccount1.Balance, updatedAccount2.Balance)
+	assert.Equal(t, account1.Balance, updatedAccount1.Balance)
+	assert.Equal(t, account2.Balance, updatedAccount2.Balance)
 }
