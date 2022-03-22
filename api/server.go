779cd47745bdd47e9426bba319c3c0127bd1f6a0
rtpa25 <pandaronit25@gmail.com>
2022-03-22 16:45:04 +0530

    addded

--- api/server.go
+++ api/server.go
@@ -1,16 +1,20 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/rtpa25/go_api_worflow/db/sqlc"
+	"github.com/rtpa25/go_api_worflow/token"
 )
 
 //server set's up HTTP routing for our banking service
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 //starts the server
@@ -19,7 +23,11 @@ func (server *Server) Start(address string) error {
 }
 
 //newServer creates a new http server and set's up routing
-func NewServer(store db.Store) *Server {
+func NewServer(store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker("")
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
 	server := &Server{store: store}
 	router := gin.Default()
 
